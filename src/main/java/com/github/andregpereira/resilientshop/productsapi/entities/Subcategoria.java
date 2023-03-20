72513439951a34da96b324b4271acb0550637d70
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-03-20 12:48:00 -0300

    Criar classe Subcategoria

---
+++ src/main/java/com/github/andregpereira/resilientshop/productsapi/entities/Subcategoria.java
@@ -0,0 +1,35 @@
+package com.github.andregpereira.resilientshop.productsapi.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_subcategorias")
+@SequenceGenerator(name = "subcategoria", sequenceName = "sq_subcategorias", allocationSize = 1)
+public class Subcategoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "subcategoria")
+	@Column(name = "id_subcategoria")
+	private Long id;
+
+	@Column(nullable = false, length = 45)
+	private String nome;
+
+	@Column(nullable = false, length = 45)
+	private String descricao;
+
+}
