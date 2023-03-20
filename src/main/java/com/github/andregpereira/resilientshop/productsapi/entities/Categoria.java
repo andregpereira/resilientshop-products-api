8ba87bf4a1b9247721e0fd03b2ca6e11bc61b9fe
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-03-20 12:47:41 -0300

    Criar classe Categoria

---
+++ src/main/java/com/github/andregpereira/resilientshop/productsapi/entities/Categoria.java
@@ -0,0 +1,39 @@
+package com.github.andregpereira.resilientshop.productsapi.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_categorias")
+@SequenceGenerator(name = "categoria", sequenceName = "sq_categorias", allocationSize = 1)
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "categoria")
+	@Column(name = "id_categoria")
+	private Long id;
+
+	@Column(nullable = false, length = 45)
+	private String nome;
+
+	@ManyToOne
+	@JoinColumn(name = "id_subcategoria", foreignKey = @ForeignKey(name = "fk_id_subcategoria"))
+	private Subcategoria subcategoria;
+
+}
