05718c7673ebd04635ae4225e1bf1d65fd3c8f44
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-03-22 11:50:53 -0300

    Criar mapper Subcategoria

--- src/main/java/com/github/andregpereira/resilientshop/productsapi/mappers/SubcategoriaMapper.java
+++ src/main/java/com/github/andregpereira/resilientshop/productsapi/mappers/SubcategoriaMapper.java
@@ -3,6 +3,7 @@ package com.github.andregpereira.resilientshop.productsapi.mappers;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
+import com.github.andregpereira.resilientshop.productsapi.dtos.subcategoria.SubcategoriaDetalhesDto;
 import com.github.andregpereira.resilientshop.productsapi.dtos.subcategoria.SubcategoriaDto;
 import com.github.andregpereira.resilientshop.productsapi.dtos.subcategoria.SubcategoriaRegistroDto;
 import com.github.andregpereira.resilientshop.productsapi.entities.Subcategoria;
@@ -15,5 +16,7 @@ public interface SubcategoriaMapper {
 	Subcategoria toSubcategoria(SubcategoriaRegistroDto dto);
 
 	SubcategoriaDto toSubcategoriaDto(Subcategoria subcategoria);
+
+	SubcategoriaDetalhesDto toSubcategoriaDetalhesDto(Subcategoria subcategoria);
 
 }
