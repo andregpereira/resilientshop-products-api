0a85f30813f415eadd56c5bfe776728a099913bc
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-03-22 17:16:25 -0300

    Produto recebe Subcategoria

--- src/main/java/com/github/andregpereira/resilientshop/productsapi/dtos/produto/ProdutoDetalhesDto.java
+++ src/main/java/com/github/andregpereira/resilientshop/productsapi/dtos/produto/ProdutoDetalhesDto.java
@@ -5,11 +5,12 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import com.github.andregpereira.resilientshop.productsapi.dtos.subcategoria.SubcategoriaDto;
 
 import lombok.Builder;
 
 @Builder
 public record ProdutoDetalhesDto(Long id, Long sku, String nome, String descricao,
 		@JsonFormat(shape = Shape.STRING, pattern = "dd/MM/yyyy - HH:mm") LocalDateTime dataCriacao,
-		BigDecimal valorUnitario, int estoque) {
+		BigDecimal valorUnitario, int estoque, SubcategoriaDto subcategoria) {
 }
