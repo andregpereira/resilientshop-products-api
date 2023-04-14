5cc333a753f8082e5f299fc1e3437f54d4004b6e
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-04-14 17:05:01 -0300

    Adicionar configurações do Spring Cloud

--- src/main/java/com/github/andregpereira/resilientshop/productsapi/ProductsapiApplication.java
+++ src/main/java/com/github/andregpereira/resilientshop/productsapi/ProductsapiApplication.java
@@ -2,8 +2,12 @@ package com.github.andregpereira.resilientshop.productsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class ProductsapiApplication {
 
     public static void main(String[] args) {
