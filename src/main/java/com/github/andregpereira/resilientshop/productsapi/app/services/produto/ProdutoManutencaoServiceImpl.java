dec23c7eb2daa77258b3945cd04f1774e6242bdb
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-04-28 20:26:18 -0300

    Refatorar código - deixar mais funcional

--- src/main/java/com/github/andregpereira/resilientshop/productsapi/app/services/produto/ProdutoManutencaoServiceImpl.java
+++ src/main/java/com/github/andregpereira/resilientshop/productsapi/app/services/produto/ProdutoManutencaoServiceImpl.java
@@ -3,11 +3,11 @@ package com.github.andregpereira.resilientshop.productsapi.app.services.produto;
 import com.github.andregpereira.resilientshop.productsapi.app.dtos.produto.ProdutoAtualizacaoDto;
 import com.github.andregpereira.resilientshop.productsapi.app.dtos.produto.ProdutoDetalhesDto;
 import com.github.andregpereira.resilientshop.productsapi.app.dtos.produto.ProdutoRegistroDto;
-import com.github.andregpereira.resilientshop.productsapi.infra.entities.Produto;
 import com.github.andregpereira.resilientshop.productsapi.cross.exceptions.ProdutoAlreadyExistsException;
 import com.github.andregpereira.resilientshop.productsapi.cross.exceptions.ProdutoNotFoundException;
 import com.github.andregpereira.resilientshop.productsapi.cross.exceptions.SubcategoriaNotFoundException;
 import com.github.andregpereira.resilientshop.productsapi.cross.mappers.ProdutoMapper;
+import com.github.andregpereira.resilientshop.productsapi.infra.entities.Produto;
 import com.github.andregpereira.resilientshop.productsapi.infra.repositories.ProdutoRepository;
 import com.github.andregpereira.resilientshop.productsapi.infra.repositories.SubcategoriaRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +15,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.text.MessageFormat;
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -35,51 +35,53 @@ public class ProdutoManutencaoServiceImpl implements ProdutoManutencaoService {
         } else if (produtoRepository.existsByNome(dto.nome())) {
             log.info("Produto já cadastrado com o nome {}", dto.nome());
             throw new ProdutoAlreadyExistsException("Opa! Já existe um produto com esse nome registrado");
-        } else if (!subcategoriaRepository.existsById(dto.idSubcategoria())) {
-            log.info("Subcategoria não encontrada com id {}", dto.idSubcategoria());
-            throw new SubcategoriaNotFoundException(
-                    "Ops! Não foi possível encontrar uma subcategoria com o id " + dto.idSubcategoria());
         }
-        Produto produto = mapper.toProduto(dto);
-        produto.setDataCriacao(LocalDateTime.now());
-        produto.setSubcategoria(subcategoriaRepository.getReferenceById(dto.idSubcategoria()));
-        produto = produtoRepository.save(produto);
-        log.info("Produto criado");
-        return mapper.toProdutoDetalhesDto(produto);
+        return subcategoriaRepository.findById(dto.idSubcategoria()).map(sc -> {
+            Produto produto = mapper.toProduto(dto);
+            produto.setDataCriacao(LocalDateTime.now());
+            produto.setSubcategoria(subcategoriaRepository.getReferenceById(dto.idSubcategoria()));
+            produto = produtoRepository.save(produto);
+            log.info("Produto criado");
+            return mapper.toProdutoDetalhesDto(produto);
+        }).orElseThrow(() -> {
+            log.info("Subcategoria não encontrada com id {}", dto.idSubcategoria());
+            return new SubcategoriaNotFoundException(
+                    "Ops! Não foi possível encontrar uma subcategoria com o id " + dto.idSubcategoria());
+        });
     }
 
     public ProdutoDetalhesDto atualizar(Long id, ProdutoAtualizacaoDto dto) {
-        Optional<Produto> optionalProduto = produtoRepository.findById(id);
-        if (optionalProduto.isEmpty()) {
+        return produtoRepository.findById(id).map(produtoAntigo -> {
+            if (produtoRepository.existsByNome(dto.nome())) {
+                log.info("Produto já cadastrado com o nome {}", dto.nome());
+                throw new ProdutoAlreadyExistsException("Opa! Já existe um produto com esse nome registrado");
+            } else if (!subcategoriaRepository.existsById(dto.idSubcategoria())) {
+                log.info("Subcategoria não encontrada com id {}", dto.idSubcategoria());
+                throw new SubcategoriaNotFoundException(dto.idSubcategoria());
+            }
+            Produto produtoAtualizado = mapper.toProduto(dto);
+            produtoAtualizado.setId(id);
+            produtoAtualizado.setSku(produtoAntigo.getSku());
+            produtoAtualizado.setDataCriacao(produtoAntigo.getDataCriacao());
+            produtoAtualizado.setSubcategoria(subcategoriaRepository.getReferenceById(dto.idSubcategoria()));
+            produtoRepository.save(produtoAtualizado);
+            log.info("Produto com id {} atualizado", id);
+            return mapper.toProdutoDetalhesDto(produtoAtualizado);
+        }).orElseThrow(() -> {
             log.info("Produto não encontrado com id {}", id);
-            throw new ProdutoNotFoundException("Ops! Não foi possível encontrar um produto com o id " + id);
-        } else if (produtoRepository.existsByNome(dto.nome())) {
-            log.info("Produto já cadastrado com o nome {}", dto.nome());
-            throw new ProdutoAlreadyExistsException("Opa! Já existe um produto com esse nome registrado");
-        } else if (!subcategoriaRepository.existsById(dto.idSubcategoria())) {
-            log.info("Subcategoria não encontrada com id {}", dto.idSubcategoria());
-            throw new SubcategoriaNotFoundException(
-                    "Ops! Não foi possível encontrar uma subcategoria com o id " + dto.idSubcategoria());
-        }
-        Produto produtoAntigo = optionalProduto.get();
-        Produto produtoAtualizado = mapper.toProduto(dto);
-        produtoAtualizado.setId(id);
-        produtoAtualizado.setSku(produtoAntigo.getSku());
-        produtoAtualizado.setDataCriacao(produtoAntigo.getDataCriacao());
-        produtoAtualizado.setSubcategoria(subcategoriaRepository.getReferenceById(dto.idSubcategoria()));
-        Produto produto = produtoRepository.save(produtoAtualizado);
-        log.info("Produto com id {} atualizado", id);
-        return mapper.toProdutoDetalhesDto(produto);
+            return new ProdutoNotFoundException(id);
+        });
     }
 
     public String remover(Long id) {
-        produtoRepository.findById(id).ifPresentOrElse(p -> produtoRepository.deleteById(id), () -> {
+        return produtoRepository.findById(id).map(p -> {
+            produtoRepository.deleteById(id);
+            log.info("Produto com id {} removido", id);
+            return MessageFormat.format("Produto com id {0} removido com sucesso", id);
+        }).orElseThrow(() -> {
             log.info("Produto não encontrado com id {}", id);
-            throw new ProdutoNotFoundException(
-                    "Desculpe, não foi possível encontrar um produto com o id " + id + ". Verifique e tente novamente");
+            return new ProdutoNotFoundException(id);
         });
-        log.info("Produto com id {} removido", id);
-        return "Produto removido";
     }
 
 }
