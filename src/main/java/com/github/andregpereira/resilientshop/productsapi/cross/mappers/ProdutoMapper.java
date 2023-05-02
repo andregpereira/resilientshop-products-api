20642291a9e48d1e536638a8eb746790ecc666f2
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-02 11:48:51 -0300

    Interligar mappers

--- src/main/java/com/github/andregpereira/resilientshop/productsapi/cross/mappers/ProdutoMapper.java
+++ src/main/java/com/github/andregpereira/resilientshop/productsapi/cross/mappers/ProdutoMapper.java
@@ -5,11 +5,13 @@ import com.github.andregpereira.resilientshop.productsapi.app.dtos.produto.Produ
 import com.github.andregpereira.resilientshop.productsapi.app.dtos.produto.ProdutoDto;
 import com.github.andregpereira.resilientshop.productsapi.app.dtos.produto.ProdutoRegistroDto;
 import com.github.andregpereira.resilientshop.productsapi.infra.entities.Produto;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants.ComponentModel;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = ComponentModel.SPRING, uses = SubcategoriaMapper.class,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ProdutoMapper {
 
     Produto toProduto(ProdutoRegistroDto dto);
--- src/main/java/com/github/andregpereira/resilientshop/productsapi/cross/mappers/SubcategoriaMapper.java
+++ src/main/java/com/github/andregpereira/resilientshop/productsapi/cross/mappers/SubcategoriaMapper.java
@@ -4,11 +4,13 @@ import com.github.andregpereira.resilientshop.productsapi.app.dtos.subcategoria.
 import com.github.andregpereira.resilientshop.productsapi.app.dtos.subcategoria.SubcategoriaDto;
 import com.github.andregpereira.resilientshop.productsapi.app.dtos.subcategoria.SubcategoriaRegistroDto;
 import com.github.andregpereira.resilientshop.productsapi.infra.entities.Subcategoria;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants.ComponentModel;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = ComponentModel.SPRING, uses = CategoriaMapper.class,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface SubcategoriaMapper {
 
     Subcategoria toSubcategoria(SubcategoriaRegistroDto dto);
