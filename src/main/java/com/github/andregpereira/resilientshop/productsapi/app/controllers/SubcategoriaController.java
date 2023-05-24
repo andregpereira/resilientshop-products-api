379cc28a790935786b637aae7353885f8cd95c26
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-24 11:53:41 -0300

    Criar Javadoc SubcategoriaController

--- src/main/java/com/github/andregpereira/resilientshop/productsapi/app/controllers/SubcategoriaController.java
+++ src/main/java/com/github/andregpereira/resilientshop/productsapi/app/controllers/SubcategoriaController.java
@@ -18,27 +18,53 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 
+/**
+ * Controller de subcategorias da API de Produtos.
+ *
+ * @author André Garcia
+ */
 @RequiredArgsConstructor
 @Slf4j
 @RestController
 @RequestMapping("/subcategorias")
 public class SubcategoriaController {
 
+    /**
+     * Injeção da dependência {@link SubcategoriaManutencaoService} para serviços de manutenção.
+     */
     private final SubcategoriaManutencaoService manutencaoService;
+
+    /**
+     * Injeção da dependência {@link SubcategoriaConsultaService} para serviços de consulta.
+     */
     private final SubcategoriaConsultaService consultaService;
 
-    // Registrar subcategoria
+    /**
+     * Cadastra uma {@linkplain SubcategoriaRegistroDto subcategoria}.
+     * Retorna uma {@linkplain SubcategoriaDetalhesDto subcategoria detalhada}.
+     *
+     * @param dto a subcategoria a ser cadastrada.
+     *
+     * @return a subcategoria criada.
+     */
     @PostMapping
-    public ResponseEntity<SubcategoriaDetalhesDto> registrar(@RequestBody @Valid SubcategoriaRegistroDto dto,
-            UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<SubcategoriaDetalhesDto> registrar(@RequestBody @Valid SubcategoriaRegistroDto dto) {
         log.info("Criando subcategoria...");
         SubcategoriaDetalhesDto subcategoria = manutencaoService.registrar(dto);
-        URI uri = uriBuilder.path("/categorias/{id}").buildAndExpand(subcategoria.id()).toUri();
+        URI uri = UriComponentsBuilder.fromPath("/categorias/{id}").buildAndExpand(subcategoria.id()).toUri();
         log.info("Subcategoria criada com sucesso");
         return ResponseEntity.created(uri).body(subcategoria);
     }
 
-    // Atualizar subcategoria por id
+    /**
+     * Atualiza uma {@linkplain SubcategoriaRegistroDto subcategoria} por {@code id}.
+     * Retorna uma {@linkplain SubcategoriaDetalhesDto subcategoria detalhada}.
+     *
+     * @param id  o id da subcategoria a ser atualizada.
+     * @param dto a subcategoria a ser atualizada.
+     *
+     * @return a subcategoria atualizada.
+     */
     @PutMapping("/{id}")
     public ResponseEntity<SubcategoriaDetalhesDto> atualizar(@PathVariable Long id,
             @RequestBody @Valid SubcategoriaRegistroDto dto) {
@@ -46,14 +72,27 @@ public class SubcategoriaController {
         return ResponseEntity.ok(manutencaoService.atualizar(id, dto));
     }
 
-    // Remover por id
+    /**
+     * Remove uma subcategoria por {@code id}. Retorna uma mensagem de confirmação de remoção.
+     *
+     * @param id o id da subcategoria a ser removida.
+     *
+     * @return uma mensagem de confirmação de remoção.
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<String> remover(@PathVariable Long id) {
         log.info("Removendo subcategoria com id {}...", id);
         return ResponseEntity.ok(manutencaoService.remover(id));
     }
 
-    // Listar todas as subcategorias
+    /**
+     * Lista todas as subcategorias cadastradas.
+     * Retorna uma {@linkplain Page sublista} de {@linkplain SubcategoriaDto subcategorias}.
+     *
+     * @param pageable o pageable padrão.
+     *
+     * @return uma sublista de uma lista com todas as subcategorias cadastradas.
+     */
     @GetMapping
     public ResponseEntity<Page<SubcategoriaDto>> listar(
             @PageableDefault(sort = "id", direction = Direction.ASC, page = 0, size = 10) Pageable pageable) {
@@ -61,7 +100,14 @@ public class SubcategoriaController {
         return ResponseEntity.ok(consultaService.listar(pageable));
     }
 
-    // Pesquisar por id
+    /**
+     * Pesquisa uma subcategoria pelo {@code id}.
+     * Retorna uma {@linkplain SubcategoriaDetalhesDto subcategoria detalhada}.
+     *
+     * @param id o id da subcategoria a ser consultada.
+     *
+     * @return uma subcategoria encontrada pelo {@code id}.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<SubcategoriaDetalhesDto> consultarPorId(@PathVariable Long id) {
         log.info("Procurando subcategoria com id {}...", id);
