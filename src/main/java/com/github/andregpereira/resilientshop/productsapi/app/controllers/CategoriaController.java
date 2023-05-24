b86b9195170f4ba1cbc9b9159b17d6a698c7c6f8
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-24 11:53:53 -0300

    Criar Javadoc CategoriaController

--- src/main/java/com/github/andregpereira/resilientshop/productsapi/app/controllers/CategoriaController.java
+++ src/main/java/com/github/andregpereira/resilientshop/productsapi/app/controllers/CategoriaController.java
@@ -17,41 +17,80 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 
+/**
+ * Controller de categorias da API de Produtos.
+ *
+ * @author André Garcia
+ */
 @RequiredArgsConstructor
 @Slf4j
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaController {
 
+    /**
+     * Injeção da dependência {@link CategoriaManutencaoService} para serviços de manutenção.
+     */
     private final CategoriaManutencaoService manutencaoService;
+
+    /**
+     * Injeção da dependência {@link CategoriaConsultaService} para serviços de consulta.
+     */
     private final CategoriaConsultaService consultaService;
 
-    // Registrar categoria
+    /**
+     * Cadastra uma {@linkplain CategoriaRegistroDto categoria}.
+     * Retorna uma {@linkplain CategoriaDto categoria}.
+     *
+     * @param dto a categoria a ser cadastrada.
+     *
+     * @return a categoria criada.
+     */
     @PostMapping
-    public ResponseEntity<CategoriaDto> registrar(@RequestBody @Valid CategoriaRegistroDto dto,
-            UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<CategoriaDto> registrar(@RequestBody @Valid CategoriaRegistroDto dto) {
         log.info("Criando categoria...");
         CategoriaDto categoria = manutencaoService.registrar(dto);
-        URI uri = uriBuilder.path("/categorias/{id}").buildAndExpand(categoria.id()).toUri();
+        URI uri = UriComponentsBuilder.fromPath("/categorias/{id}").buildAndExpand(categoria.id()).toUri();
         log.info("Categoria criada com sucesso");
         return ResponseEntity.created(uri).body(categoria);
     }
 
-    // Atualizar categoria por id
+    /**
+     * Atualiza uma {@linkplain CategoriaRegistroDto categoria} por {@code id}.
+     * Retorna uma {@linkplain CategoriaDto categoria}.
+     *
+     * @param id  o id da categoria a ser atualizada.
+     * @param dto a categoria a ser atualizada.
+     *
+     * @return a categoria atualizada.
+     */
     @PutMapping("/{id}")
     public ResponseEntity<CategoriaDto> atualizar(@PathVariable Long id, @RequestBody @Valid CategoriaRegistroDto dto) {
         log.info("Atualizando categoria com id {}...", id);
         return ResponseEntity.ok(manutencaoService.atualizar(id, dto));
     }
 
-    // Remover por id
+    /**
+     * Remove uma categoria por {@code id}. Retorna uma mensagem de confirmação de remoção.
+     *
+     * @param id o id da categoria a ser removida.
+     *
+     * @return uma mensagem de confirmação de remoção.
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<String> remover(@PathVariable Long id) {
         log.info("Removendo categoria com id {}...", id);
         return ResponseEntity.ok(manutencaoService.remover(id));
     }
 
-    // Listar todas as categorias
+    /**
+     * Lista todas as subcategorias cadastradas.
+     * Retorna uma {@linkplain Page sublista} de {@linkplain CategoriaDto categorias}.
+     *
+     * @param pageable o pageable padrão.
+     *
+     * @return uma sublista de uma lista com todas as subcategorias cadastradas.
+     */
     @GetMapping
     public ResponseEntity<Page<CategoriaDto>> listar(
             @PageableDefault(sort = "id", direction = Direction.ASC, page = 0, size = 10) Pageable pageable) {
@@ -59,7 +98,14 @@ public class CategoriaController {
         return ResponseEntity.ok(consultaService.listar(pageable));
     }
 
-    // Pesquisar por id
+    /**
+     * Pesquisa uma categoria pelo {@code id}.
+     * Retorna uma {@linkplain CategoriaDto categoria}.
+     *
+     * @param id o id da categoria a ser consultada.
+     *
+     * @return uma categoria encontrada pelo {@code id}.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<CategoriaDto> consultarPorId(@PathVariable Long id) {
         log.info("Procurando categoria com id {}...", id);
