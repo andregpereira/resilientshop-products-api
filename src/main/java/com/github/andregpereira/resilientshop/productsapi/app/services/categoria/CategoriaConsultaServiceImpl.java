1dae8ef55728ab2e68db6b316a99efac47fe8b04
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-25 10:14:55 -0300

    Criar Javadoc CategoriaConsultaService

--- src/main/java/com/github/andregpereira/resilientshop/productsapi/app/services/categoria/CategoriaConsultaServiceImpl.java
+++ src/main/java/com/github/andregpereira/resilientshop/productsapi/app/services/categoria/CategoriaConsultaServiceImpl.java
@@ -11,14 +11,39 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+/**
+ * Classe de serviço de consulta de {@link Categoria}.
+ *
+ * @author André Garcia
+ * @see CategoriaConsultaService
+ */
 @RequiredArgsConstructor
 @Slf4j
 @Service
 public class CategoriaConsultaServiceImpl implements CategoriaConsultaService {
 
+    /**
+     * Injeção da dependência {@link CategoriaRepository} para realizar operações de
+     * consulta na tabela de categorias no banco de dados.
+     */
     private final CategoriaRepository repository;
+
+    /**
+     * Injeção da dependência {@link CategoriaMapper} para realizar
+     * conversões de entidade para DTO de categorias.
+     */
     private final CategoriaMapper mapper;
 
+    /**
+     * Lista todas as {@linkplain Categoria categorias} cadastradas.
+     * Retorna uma {@linkplain Page sublista} de {@linkplain CategoriaDto categorias}.
+     *
+     * @param pageable o pageable padrão.
+     *
+     * @return uma sublista de uma lista com todas as categorias cadastradas.
+     *
+     * @throws CategoriaNotFoundException caso nenhuma categoria seja encontrada.
+     */
     @Override
     public Page<CategoriaDto> listar(Pageable pageable) {
         Page<Categoria> categorias = repository.findAll(pageable);
@@ -30,6 +55,16 @@ public class CategoriaConsultaServiceImpl implements CategoriaConsultaService {
         return categorias.map(mapper::toCategoriaDto);
     }
 
+    /**
+     * Pesquisa uma {@linkplain Categoria categoria} por {@code id}.
+     * Retorna uma {@linkplain  CategoriaDto categoria}.
+     *
+     * @param id o id da categoria.
+     *
+     * @return uma categoria encontrada pelo {@code id}.
+     *
+     * @throws CategoriaNotFoundException caso a categoria não seja encontrada.
+     */
     @Override
     public CategoriaDto consultarPorId(Long id) {
         return repository.findById(id).map(c -> {
